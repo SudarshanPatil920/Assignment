413462a8244408c2e1977b894b81625f2366ac21
Sudarshan Patil <sudarshanpatil486@gmail.com>
2025-09-24 12:01:59 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
